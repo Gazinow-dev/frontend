4481efb2be12c8bc6c7efe494a67beaac5e6c32f
ryd10227 <ryd10227@gmail.com>
2025-04-25 17:12:55 +0900

    전체 페이지 불러오기 오류 화면 추가

---
+++ appFrontend/src/global/components/NetworkErrorScreen.tsx
@@ -0,0 +1,41 @@
+import { SafeAreaView, TouchableOpacity, View } from 'react-native';
+import { FontText } from '@global/ui';
+import IconLeftArrowHead from '@assets/icons/left_arrow_head.svg';
+import { useRootNavigation } from '@/navigation/RootNavigation';
+
+interface NetworkErrorScreenProps {
+  retryFn: () => void;
+  isShowBackBtn?: boolean;
+}
+
+const NetworkErrorScreen = ({ retryFn, isShowBackBtn }: NetworkErrorScreenProps) => {
+  const navigation = useRootNavigation();
+  return (
+    <SafeAreaView className="flex-1 bg-white">
+      {isShowBackBtn && (
+        <TouchableOpacity className="p-16 w-30" hitSlop={20} onPress={() => navigation.goBack()}>
+          <IconLeftArrowHead color="#3F3F46" width={24} />
+        </TouchableOpacity>
+      )}
+
+      <View className="items-center justify-center flex-1 bg-white">
+        <FontText
+          text="이용에 불편을 드려 죄송합니다"
+          className="text-18 leading-23 text-gray-999"
+          fontWeight="600"
+        />
+        <FontText
+          text={`일시적인 오류가 발생했어요\n다시 시도해 주세요`}
+          className="mt-8 mb-32 text-center text-gray-999"
+        />
+        <TouchableOpacity onPress={retryFn} className="px-16 py-12 w-80 bg-black-717 rounded-5">
+          <FontText text="확인" className="text-center text-white text-14" fontWeight="600" />
+        </TouchableOpacity>
+      </View>
+
+      <View className="h-100" />
+    </SafeAreaView>
+  );
+};
+
+export default NetworkErrorScreen;
