f4c3699f07f4da714e4ecd474bf6670bd42a8fcf
ryd10227 <ryd10227@gmail.com>
2025-10-08 20:34:43 +0900

    Chore: 프리티어 버전 업데이트

--- appFrontend/src/screens/myRootScreen/components/SubscribeTermsScreen.tsx
+++ appFrontend/src/screens/myRootScreen/components/SubscribeTermsScreen.tsx
@@ -6,27 +6,35 @@ import { SUBSCRIBE_TERMS_VER_1 } from '@/global/constants/terms';
 
 const SubscribeTermsScreen = () => {
   const myPageNavigation = useMyPageNavigation();
-  const handleBack=() => myPageNavigation.goBack()
+  const handleBack = () => myPageNavigation.goBack();
 
   return (
     <SafeAreaView className="flex-1 bg-white">
-      <TouchableOpacity className='p-16' onPress={handleBack}>
-          <IconChevronLeft />
+      <TouchableOpacity className="p-16" onPress={handleBack}>
+        <IconChevronLeft />
       </TouchableOpacity>
 
-      <ScrollView className='m-16 space-y-28'>
-        <FontText text='가는길지금 이용약관 ver.1' fontWeight='500' className='text-20 leading-[130%] tracking-normal' />
+      <ScrollView className="m-16 space-y-28">
+        <FontText
+          text="가는길지금 이용약관 ver.1"
+          fontWeight="500"
+          className="text-20 leading-[130%] tracking-normal"
+        />
         <View className="h-1 bg-gray-beb" />
-        {SUBSCRIBE_TERMS_VER_1.map(({title, body}, idx)=>
-          <View key={idx} className='space-y-8'>
-            <FontText text={title} fontWeight='600'/>
-            <View className='space-y-8'>
-              {body.map((detail, idx)=>
-                <FontText key={idx} text={detail} className="font-light tracking-normal leading-25"/>
-              )}
+        {SUBSCRIBE_TERMS_VER_1.map(({ title, body }, idx) => (
+          <View key={idx} className="space-y-8">
+            <FontText text={title} fontWeight="600" />
+            <View className="space-y-8">
+              {body.map((detail, idx) => (
+                <FontText
+                  key={idx}
+                  text={detail}
+                  className="font-light tracking-normal leading-25"
+                />
+              ))}
             </View>
           </View>
-        )}
+        ))}
       </ScrollView>
     </SafeAreaView>
   );
