982c922e46890cd4b28212b6e87f3436935bd9f3
ryd10227 <ryd10227@gmail.com>
2026-01-17 21:35:18 +0900

    Fix: 로그인 세션 자동 연장 reissue 에러

--- appFrontend/src/global/apis/index.ts
+++ appFrontend/src/global/apis/index.ts
@@ -49,7 +49,7 @@ authServiceAPI.interceptors.response.use(
   async (error: AxiosError) => {
     if (
       !error.response ||
-      error.response.status !== 403 ||
+      error.response.status !== 401 ||
       error.response.config.url === '/api/v1/member/reissue'
     ) {
       throw error;
