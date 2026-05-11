ded4d981afaf6a20ef2e9a741b574b46eb166c29
ryd10227 <ryd10227@gmail.com>
2026-05-12 04:12:10 +0900

    Chore: 온보딩 화면 assets 추가

--- appFrontend/src/assets/images/index.ts
+++ appFrontend/src/assets/images/index.ts
@@ -3,3 +3,5 @@ export { default as ImgWalkthroughMypath } from './img-walkthrough-mypath.svg';
 export { default as IconTextWalkthroughMypath } from './text-walkthrough-mypath.svg';
 export { default as ImgWalkthroughNoti } from './img-walkthrough-noti.svg';
 export { default as IconTextWalkthroughNoti } from './text-walkthrough-noti.svg';
+export { default as ImgOnboardingStep1 } from './img-onboarding-1.svg';
+export { default as ImgOnboardingStep2Path } from './img-onboarding-2-Path.svg';
